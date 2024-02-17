df19aedcbf718e452cea3091503e9653f039bf34
akouryy <akouryy7@gmail.com>
2024-02-18 08:33:22 +0900

    Upload out/ files to S3

--- main.tf
+++ main.tf
@@ -14,3 +14,18 @@ provider "aws" {
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = "shogir.pyon.app"
 }
+
+module "s3_files" {
+  source = "hashicorp/dir/template"
+  base_dir = "${path.module}/out"
+}
+
+resource "aws_s3_object" "s3_files_upload" {
+  for_each = module.s3_files.files
+  bucket = aws_s3_bucket.s3_bucket.id
+  key = each.key
+  content_type = each.value.content_type
+  source = each.value.source_path
+  content = each.value.content
+  source_hash = each.value.digests.md5
+}
